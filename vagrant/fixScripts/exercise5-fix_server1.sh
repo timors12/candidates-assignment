1b4f624b3878966e421decfd621f5d3c426e1290
Timor Shtein <timor.shtein@gmail.com>
2020-03-13 17:49:44 +0200

    fix_scripts_Timor_shtein

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+#i cant figure how to do the passwordles login from a script
+echo 'Host server2
+   StrictHostKeyChecking no' >> /etc/ssh/ssh_config
+echo 'Host 192.168.100.11
+   StrictHostKeyChecking no' >> /etc/ssh/ssh_config
+ssh-keygen -f /home/vagrant/.ssh/id_rsa -N '' -q
+#ssh-copy-id -i /home/vagrant/.ssh/id_rsa vagrant@server2
+#cat /home/vagrant/.ssh/id_rsa.pub | sudo ssh -i /vagrant/.vagrant/machines/server2/virtualbox/private_key vagrant@192.168.100.10 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+
--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+# create veriabels
+serverName = 'hostname' 
+reciver=server2
+totalSize=0
+
+#find what server
+if [ "$serverName" = "server2" ]; then
+reciver=server1
+fi
+
+#seperate the source pathe and destination
+declare -a args=("$@")
+pathInRemote=${args[$((${#args[@]}-1))]}
+declare-a sourcePath = ("$@")
+unset sourcePath[${#sourcePath[@]}-1]
+
+
+#run the copy using scp, this works if you no longer need a password for ssh 
+for path in $sourcePath;
+do
+    scp $path $reciver:$pathInRemote
+    totalSize=$((totalSize + $(stat -c "%s" $path)))
+done
+
+
+#prin toatl size
+echo $totalSize
